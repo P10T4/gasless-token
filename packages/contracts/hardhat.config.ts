f64a8f457cd7e923feee5f5cbdbb8e6021482cb9
sweesenkoh <sweesenworkstation@gmail.com>
2021-11-03 19:08:52 +0800

    fix commands in package.json

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,12 +1,12 @@
-import { task, HardhatUserConfig } from "hardhat/config";
-import { removeConsoleLog } from "hardhat-preprocessor";
-import "@nomiclabs/hardhat-waffle";
-import "hardhat-deploy";
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
+import { task, HardhatUserConfig } from 'hardhat/config';
+import { removeConsoleLog } from 'hardhat-preprocessor';
+import '@nomiclabs/hardhat-waffle';
+import 'hardhat-deploy';
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
   for (const account of accounts) {
     console.log(account.address);
@@ -16,16 +16,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   // defaultNetwork: "ganache",
   paths: {
-    sources: "./contracts",
-    tests: "./test",
-    artifacts: "./build/artifacts",
-    cache: "./build/cache",
-    deploy: "./scripts/deploy",
+    sources: './contracts',
+    tests: './test',
+    artifacts: './build/artifacts',
+    cache: './build/cache',
+    // deploy: "./scripts/deploy",
   },
   solidity: {
     compilers: [
       {
-        version: "0.7.6",
+        version: '0.7.6',
         settings: {
           optimizer: {
             enabled: true,
@@ -39,14 +39,11 @@ const config: HardhatUserConfig = {
     timeout: 20000,
   },
   typechain: {
-    outDir: "typechained",
-    target: "ethers-v5",
+    outDir: 'typechained',
+    target: 'ethers-v5',
   },
   preprocess: {
-    eachLine: removeConsoleLog(
-      (hre) =>
-        hre.network.name !== "hardhat" && hre.network.name !== "localhost"
-    ),
+    eachLine: removeConsoleLog((hre) => hre.network.name !== 'hardhat' && hre.network.name !== 'localhost'),
   },
   networks: {
     hardhat: {
@@ -55,27 +52,27 @@ const config: HardhatUserConfig = {
       accounts: [
         {
           privateKey: '0x2c6036688de3383aac61e305b3bf91bc567f508ce01db108d17e62ca364a2488',
-          balance: '10000000000000000000000'
+          balance: '10000000000000000000000',
         },
         {
           privateKey: '0x6d61bdbd9c2cd61f2be4b8aaf3cb930cb317028389f8f7f7f454ccc98d36d4f5',
-          balance: '10000000000000000000000'
+          balance: '10000000000000000000000',
         },
         {
           privateKey: '0x21f33ccbc687ce720efee931d205f8f7b77d40c6cd2863c1cc61fc0a472c4cc9',
-          balance: '10000000000000000000000'
-        }
-      ]
+          balance: '10000000000000000000000',
+        },
+      ],
     },
     ganache: {
       url: 'http://localhost:8545',
       chainId: 1337,
-    }
+    },
   },
   namedAccounts: {
     deployer: {
       default: 0, // by default, take the first account as deployer
-      rinkeby: "0x5238A644636946963ffeDAc52Ec53fb489D3a1CD", // on rinkeby, use a specific account
+      rinkeby: '0x5238A644636946963ffeDAc52Ec53fb489D3a1CD', // on rinkeby, use a specific account
     },
   },
 };
